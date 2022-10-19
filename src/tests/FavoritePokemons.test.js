ffd6dee61a1aab87cbf4a68a971fc1cea523c70c
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-10-19 18:24:03 -0400

    Conclui requisito N°3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste o componente Favorite', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
