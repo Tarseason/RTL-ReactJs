f77c6ab7b7fd0b649d1617cd6618d327aa64f8d3
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-10-19 15:28:49 -0400

    Corrige requisito N°1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,10 +3,22 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('Testa se existe um link na aplicação para Home About e Favorites Pokémons', () => {
-  renderWithRouter(<App />);
+describe('Teste o Componente App', () => {
+  test('Testa se existe um link na aplicação para Home', () => {
+    renderWithRouter(<App />);
 
-  expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
-  expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
-  expect(screen.getByRole('link', { name: 'Favorite Pokémons' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+  });
+
+  test('Testa se existe um link na aplicação para About', () => {
+    renderWithRouter(<App />);
+
+    expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+  });
+
+  test('Testa se existe um link na aplicação para Favorites Pokémons', () => {
+    renderWithRouter(<App />);
+
+    expect(screen.getByRole('link', { name: 'Favorite Pokémons' })).toBeInTheDocument();
+  });
 });
