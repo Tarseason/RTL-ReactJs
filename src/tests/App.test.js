c8eee89f0526456a75cb3680de98ae3e565bc680
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-10-19 14:36:51 -0400

    Conclui requisito N°1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Testa se existe um link na aplicação para Home About e Favorites Pokémons', () => {
+  renderWithRouter(<App />);
+
+  expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+  expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+  expect(screen.getByRole('link', { name: 'Favorite Pokémons' })).toBeInTheDocument();
+});
