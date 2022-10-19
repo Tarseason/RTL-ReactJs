64442e6655c9ed4528011c15c8d5a834d7b0bbad
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-10-19 18:47:54 -0400

    Conclui requisito N°4 e corrige erro do ultimo commit

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente NotFound', () => {
+  test('É exibido na tela um h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    expect(screen.getByRole('heading', { name: 'Page requested not found', level: 2 })).toBeInTheDocument();
+  });
+
+  test('Existe uma imagem com o src desejado', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', { src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif' });
+    expect(img).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
